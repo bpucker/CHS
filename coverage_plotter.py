eb9e373486ee9b7bf05d63e0af0f0366e5d2a523
Boas Pucker <36674497+bpucker@users.noreply.github.com>
2023-03-13 22:56:41 +0100

    Add files via upload

--- coverage_plotter.py
+++ coverage_plotter.py
@@ -35,13 +35,15 @@ def load_coverage( cov_file, seq, start, end ):
 	return coverage
 
 
-def coverage_plotter( output_file, coverage, seq, start, end ):
+def coverage_plotter( output_file, coverage, seq, start, end, exons ):
 	"""! @brief plot the coverage """
 	
 	fig, ax = plt.subplots( figsize=(8,3) )
 	ax.plot( coverage, color="green" )
 	ax.set_ylabel( "coverage" )
 	ax.set_xlabel( "position at locus" )
+	for exon in exons:
+		ax.plot( [ exon[0]-start, exon[1]-start ], [ 0, 0 ], color="black" )
 	plt.subplots_adjust(left=0.1, right=0.95, top=0.99, bottom=0.3)
 	fig.savefig( output_file, dpi=300 )
 
@@ -55,6 +57,24 @@ def save_data( data_file, seq, start, coverage ):
 			out.write( new_line )
 
 
+def load_relevant_exon_positions( gff_file, seq, start, end ):
+	"""! @brief load all exon positions within region of interest """
+	
+	exon_pos = []
+	with open( gff_file, "r" ) as f:
+		line = f.readline()
+		while line:
+			if line[0] != '#':
+				parts = line.strip().split('\t')
+				if parts[0] == seq:
+					if parts[2] == "exon":
+						if start < int( parts[3] ) < end:
+							x, y = int( parts[3] ), int( parts[4] )
+							exon_pos.append( [ x, y ] )
+			line = f.readline()
+	return exon_pos
+
+
 def main( arguments ):
 	"""! @brief run everything """
 	
@@ -64,8 +84,14 @@ def main( arguments ):
 	start = int( arguments[ arguments.index('--start')+1 ] )
 	end = int( arguments[ arguments.index('--end')+1 ] )
 	
+	if '--gff' in arguments:
+		gff_file = arguments[ arguments.index('--gff')+1 ]
+		exons = load_relevant_exon_positions( gff_file, seq, start, end )
+	else:
+		exons = []
+	
 	cov = load_coverage( cov_file, seq, start, end )
-	coverage_plotter( output_file, cov, seq, start, end )
+	coverage_plotter( output_file, cov, seq, start, end, exons )
 	data_file = output_file + ".data.txt"
 	save_data( data_file, seq, start, cov )
 
